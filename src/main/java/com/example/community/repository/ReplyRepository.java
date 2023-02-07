809d42eb5897eb3638f6030ce1811ae16995d80b
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2023-02-08 01:00:31 +0900

    feat : ReplyRepository 쿼리메소드 추가

--- src/main/java/com/example/community/repository/ReplyRepository.java
+++ src/main/java/com/example/community/repository/ReplyRepository.java
@@ -1,7 +1,9 @@
 package com.example.community.repository;
 
 import com.example.community.domain.reply.Reply;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
+    List<Reply> findAllByBoardId(Long boardId);
 }
